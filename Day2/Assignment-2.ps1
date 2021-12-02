98b78d33ecf78a5945db1ff38147b9484a5394f0
Mike Bijl <mike.bijl@aah.nl>
2021-12-02 11:25:33 +0100

    Day2 - Part 2 solution

---
+++ Day2/Assignment-2.ps1
@@ -0,0 +1,33 @@
+$instructions = Import-Csv input.txt -Delimiter ' ' -Header 'Direction','Distance'
+
+$verticalDistance = 0
+$horizontalDistance = 0
+$aim = 0
+
+foreach ($instruction in $instructions) {
+    switch ($instruction.Direction) {
+        "forward"   {
+            $horizontalDistance += $instruction.Distance
+            $verticalDistance   += $aim * $instruction.Distance
+        }
+        "up"        {
+            #$verticalDistance   -= $instruction.Distance
+            $aim                -= $instruction.Distance
+        }
+        "down"      {
+            #$verticalDistance   += $instruction.Distance
+            $aim                += $instruction.Distance
+        }
+        Default     {
+            #$horizontalDistance -= $instruction.Distance
+        }
+    }
+}
+write-host "Depth of the submarine is: " -NoNewline -ForegroundColor Cyan
+Write-Host $verticalDistance -ForegroundColor Magenta
+write-host "Horizontal distance of the submarine is: " -NoNewline -ForegroundColor Cyan
+Write-Host $horizontalDistance -ForegroundColor Magenta
+
+$answer = $horizontalDistance * $verticalDistance
+write-host "Answer: " -NoNewline -ForegroundColor Cyan
+Write-Host $answer -ForegroundColor Magenta
